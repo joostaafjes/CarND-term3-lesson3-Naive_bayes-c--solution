1d5fd2360805b9dd11a51e7ca77595987859222b
joost aafjes <joostaafjes@gmail.com>
2019-04-05 15:04:05 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/CarND_term3_lesson3_Naive_bayes_c___solution.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CarND_term3_lesson3_Naive_bayes_c___solution.dir/main.cpp.o"
+  "CMakeFiles/CarND_term3_lesson3_Naive_bayes_c___solution.dir/classifier.cpp.o"
+  "CarND_term3_lesson3_Naive_bayes_c___solution.pdb"
+  "CarND_term3_lesson3_Naive_bayes_c___solution"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CarND_term3_lesson3_Naive_bayes_c___solution.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
